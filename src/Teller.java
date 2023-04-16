010a1956b9af0031094a73e31a31fafea10bab33
rsrulloda <ronell.rulloda@gmail.com>
2023-04-16 11:52:42 -0700

    Updated Teller

--- src/Teller.java
+++ src/Teller.java
@@ -1,2 +1,34 @@
 public class Teller {
+
+
+    public void loginEmployee(String username, String password) {
+    }
+
+    public void loginCustomer(String username, String password) {
+    }
+
+    public void logoutEmployee() {
+    }
+
+    public void logoutCustomer() {
+    }
+
+    public void withdrawRequest(String accountNumber, double amount) {
+    }
+
+    public void depositRequest(String accountNumber, double amount) {
+    }
+
+    public void transfer(BankAccount account1, BankAccount account2, double amount) {
+    }
+
+
+    public void closeAcc(String accountNumber) {
+    }
+
+    public void openAcc() {
+    }
+
+    public void editAcc(String accountNumber, String nickname) {
+    }
 }
