474063748ead016e97f6c146e69d312be1100a06
ThucCTran <126632958+ThucCTran@users.noreply.github.com>
2023-04-30 17:27:31 -0700

    Update ATM.java

--- src/ATM.java
+++ src/ATM.java
@@ -1,3 +1,51 @@
-public class ATM {
-	private String test = "test";
+public class ATM 
+{
+	private Customer currentCustomer;
+	
+    public void loginCustomer(Message<CustomerLogin> loginMessage)
+    {
+        CustomerLogin login = loginMessage.getData();
+        currentCustomer = new Customer(login.getUsername(), login.getPassword());
+    }
+    
+    public void logoutCustomer(Message<CustomerLogout> logoutMessage)
+    {
+        currentCustomer = null;
+    }
+    
+    public void withdrawRequest(int accountNumber, double amount) 
+    {
+        if (currentCustomer != null) 
+        {
+        	
+        } 
+        else
+        {
+        	
+        }
+    }
+    
+    public void depositRequest(int accountNumber, double amount)
+    {
+        if (currentCustomer != null) 
+        {
+        	
+        } 
+        else 
+        {
+        	
+        }
+    }
+    
+    public void transfer(int account1, int account2, double amount)
+    {
+        if (currentCustomer != null)
+        {
+        	
+        } 
+        else 
+        {
+        	
+        }
+    }
 }
