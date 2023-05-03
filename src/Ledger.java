d1c347b84c9ab0adacea1ee1b6665528adedd10b
TubbsTheCat <aj.almaxlisingjr@gmail.com>
2023-05-02 22:18:22 -0700

    Implemented Ledger and LedgerEntry classes

--- src/Ledger.java
+++ src/Ledger.java
@@ -1,67 +1,29 @@
+import java.util.ArrayList;
 import java.util.Date;
-import java.io.FileWriter; 
-import java.io.IOException;
-
 
 public class Ledger {
-	
-		private static final String LEDGER_FORMAT = "%s %s %s %s      %s";
-		
-		public float amnt;			
-		public Date time;		
-		public String typ;
-		public int account;
-		public int counter=0;
-		
-		public Ledger(Date open, int accNum, float amount, String type ) {
-			
-			amnt = amount;
-			time = open;
-			typ = type;
-			account=accNum;
-			counter++;
-			
-		}
-		
-	
-		public String toFile() {
-			
-			
-			
-			String text= String.format(LEDGER_FORMAT,counter, time, typ,account, "$"+amnt );
-			 
-			try {
-				 
-		            // Create a FileWriter object
-		            // to write in the file
-		            FileWriter fWriter = new FileWriter(
-		                "filename");
-		 
-		            // Writing into file
-		            // Note: The content taken above inside the
-		            // string
-		            fWriter.write(text);
-		 
-		            // Printing the contents of a file
-		            System.out.println(text);
-		 
-		            // Closing the file writing connection
-		            fWriter.close();
-		 
-		            // Display message for successful execution of
-		            // program on the console
-		            System.out.println(
-		                "File is created successfully with the content.");
-		            
-		        }
-		 
-		        // Catch block to handle if exception occurs
-		        catch (IOException e) {
-		 
-		            // Print the exception
-		            System.out.print(e.getMessage());
-		        }
-		}
-		
-
+    
+    private final ArrayList<LedgerEntry> ledgerEntries;
+    
+    public Ledger() {
+        ledgerEntries = new ArrayList<>();
+    }
+    
+    public void addEntry(float amountChanged, String customerAccount, String accountNumber, String location, Date date) {
+        LedgerEntry entry = new LedgerEntry(amountChanged, customerAccount, accountNumber, location, date);
+        ledgerEntries.add(entry);
+    }
+    
+    public void addEntry(float amountChanged, String customerAccount, String accountNumber, String location, Date date, String bankEmployee) {
+        LedgerEntry entry = new LedgerEntry(amountChanged, customerAccount, accountNumber, location, date, bankEmployee);
+        ledgerEntries.add(entry);
+    }
+    
+    public void loadEntries(String filename) {
+    	
+    }
+    
+    public ArrayList<LedgerEntry> getLedgerEntries() {
+        return ledgerEntries;
+    }
 }
--- src/LedgerEntry.java
+++ src/LedgerEntry.java
@@ -1,2 +1,60 @@
+import java.util.Date;
+
 public class LedgerEntry {
+    
+    private final float amountChanged;
+    private final String customerAccount;
+    private final String accountNumber;
+    private final String location;
+    private final Date date;
+    private final String details;
+    private final String bankEmployee;
+    
+    public LedgerEntry(float amountChanged, String customerAccount, String accountNumber, String location, Date date) {
+        this.amountChanged = amountChanged;
+        this.customerAccount = customerAccount;
+        this.accountNumber = accountNumber;
+        this.location = location;
+        this.date = date;
+		this.details = "";
+		this.bankEmployee = "";
+    }
+    
+    public LedgerEntry(float amountChanged, String customerAccount, String accountNumber, String location, Date date, String bankEmployee) {
+        this.amountChanged = amountChanged;
+        this.customerAccount = customerAccount;
+        this.accountNumber = accountNumber;
+        this.location = location;
+        this.date = date;
+		this.details = "";
+        this.bankEmployee = bankEmployee;
+    }
+    
+    public float getAmountChanged() {
+        return amountChanged;
+    }
+    
+    public String getCustomerAccount() {
+        return customerAccount;
+    }
+    
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    
+    public String getLocation() {
+        return location;
+    }
+    
+    public Date getDate() {
+        return date;
+    }
+    
+    public String getDetails() {
+        return details;
+    }
+    
+    public String getBankEmployee() {
+        return bankEmployee;
+    }
 }
